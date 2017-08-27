9420587e28c90b8d4eb5bb1ddc44c92f7d38795b
James Ralphs <jr39.dev@gmail.com>
2017-08-27 23:41:17 +0200

    redesign listing page

--- client/TrackListingView.js
+++ client/TrackListingView.js
@@ -3,43 +3,102 @@ define([
   "jquery"
 ], function(Handlebars, $) {
 
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  var daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
   var trackListingTemplate = Handlebars.compile([
-    "<table class='table'>",
-      "<tbody>",
-        "{{#trackSummaries}}",
-          "<tr>",
-            "<td>",
+    "{{#trackSummaryDates}}",
+      "<div>",
+        "<div>",
+        "{{dateStr}}",
+        "</div>",
+        "<div style='overflow:auto'>",
+          "{{#trackSummaries}}",
+            "<div style='float: left; margin: 5px; background-color: lightblue'>",
               "<a href='#/track/{{encodedTrackName}}'>",
-                "{{startTime}}",
+                "<p>{{startTimeStr}}<p>",
               "</a>",
-            "</td>",
-            "<td>{{distanceKm}} Km</td>",
-          "</tr>",
-        "{{/trackSummaries}}",
-      "</tbody>",
-    "</table>"
+              "<p>{{distanceKm}} Km</p>",
+            "</div>",
+          "{{/trackSummaries}}",
+        "</div>",
+    "</div>",
+  "{{/trackSummaryDates}}",
   ].join(""));
 
   return function() {
     return {
       render: function($element) {
         $.ajax("/api/fit/").then(function(summaries) {
-          var summaries = summaries.map(function(summary) {
-            return Object.assign({}, summary, {
-              encodedTrackName: encodeURIComponent(summary.name),
-              startTimestampMs: new Date(summary.startTime).getTime(),
-              distanceKm: summary.totalDistance / 1000
-            });
+          var trackSummaryDatesMap = {}; // [] dayTs[tracks]
+
+          summaries.forEach(function(summary) {
+            var summaryTs = new Date(summary.startTime);
+            var dayTs = new Date(summaryTs.getFullYear(), summaryTs.getMonth(), summaryTs.getDate()).getTime();
+
+            if (!trackSummaryDatesMap[dayTs]) {
+              trackSummaryDatesMap[dayTs] = [];
+            }
+
+            trackSummaryDatesMap[dayTs].push(summary)
           });
-          summaries.sort(function(a, b){
-            if (a.startTimestampMs < b.startTimestampMs) {
+
+          var trackSummaryDates = Object.keys(trackSummaryDatesMap).map(function(dayTs) {
+            dayTs = parseInt(dayTs, 10);
+            var date = new Date(dayTs);
+
+            return {
+              dayTs: dayTs,
+              dateStr: daysOfWeek[date.getDay()] + ", " + date.getDate() + " " + monthNames[date.getMonth()] + " " + date.getFullYear(),
+              trackSummaries: trackSummaryDatesMap[dayTs].map(function(summary) {
+                var startDate = new Date(summary.startTime);
+
+                return Object.assign({}, summary, {
+                  encodedTrackName: encodeURIComponent(summary.name),
+                  startTimestampMs: new Date(summary.startTime).getTime(),
+                  distanceKm: (summary.totalDistance / 1000).toFixed(2),
+                  startTimeStr: startDate.getHours() + ":" + startDate.getMinutes()
+                });
+              }).sort(function(a, b){
+                if (a.startTimestampMs < b.startTimestampMs) {
+                  return 1;
+                }
+                return -1;
+              })
+            };
+          });
+
+          trackSummaryDates.sort(function(a, b) {
+            if (a.dayTs < b.dayTs) {
               return 1;
             }
             return -1;
           });
 
           $element.html(trackListingTemplate({
-            trackSummaries: summaries
+            trackSummaryDates: trackSummaryDates
           }));
         }).fail(function(){
           throw new Error("error"); // todo handle
