8385ffb66c71da231b52ae49d06eb446aeb23ae0
James Ralphs <jr39.dev@gmail.com>
2017-09-21 18:39:59 +0200

    lookup a location with the overpass api

--- server/domain/fit_file.go
+++ server/domain/fit_file.go
@@ -8,13 +8,6 @@ import (
 	"github.com/tormoder/fit"
 )
 
-type ActivityBounds struct {
-	LatMin  float64 `json:"latMin"`  // between -90 (south pole) and +90 (north pole)
-	LatMax  float64 `json:"latMax"`  // between -90 (south pole) and +90 (north pole)
-	LongMin float64 `json:"longMin"` // between -180 and +180
-	LongMax float64 `json:"longMax"` // between -180 and +180
-}
-
 type FitFile struct {
 	*FitFileSummary `json:"summary"`
 	Records         []*Record `json:"records"`
---
+++ server/domain/geographic_map_element.go
@@ -0,0 +1,7 @@
+package domain
+
+type GeographicMapElement struct {
+	Name  string `json:"name"`
+	Place string `json:"place"`
+	IsIn  string `json:"isIn"`
+}
