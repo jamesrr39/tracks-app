d952cdfced25be770c84e242b05dced939cd405f
James <jr39.dev@gmail.com>
2018-02-06 15:14:44 +0100

    refactor class structure

---
+++ client/src/domain/trackGroup.ts
@@ -0,0 +1,32 @@
+import { TrackSummary } from "./track";
+import { formatDuration } from "./duration";
+
+export class Group {
+  constructor(
+    public readonly name: string,
+    public readonly trackSummaries: TrackSummary[]){}
+
+  getTotalDistanceKm() :string {
+    const distanceMetres = this.trackSummaries.map((trackSummary) => {
+      return trackSummary.totalDistance;
+    }).reduce((prev, thisVal) => {
+      return prev + thisVal;
+    });
+
+    return (distanceMetres / 1000).toFixed(2);
+  }
+
+  getTotalDurationStr() :string {
+    const durationSeconds = this.trackSummaries.map((trackSummary) => {
+      return trackSummary.getDurationSeconds();
+    }).reduce((prev, thisVal) => {
+      return prev + thisVal;
+    });
+
+    return formatDuration(durationSeconds);
+  }
+}
+
+export interface GroupedTrackSummaries {
+  getGroupsAscending(): Group[]
+}
